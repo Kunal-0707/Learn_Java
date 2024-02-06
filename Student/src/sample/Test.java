047770a4a6b9d417c3a08b81dd917ef33fe1b74d
Kunal-0707 <156047174+Kunal-0707@users.noreply.github.com>
2024-02-06 12:59:16 +0530

    Simple Student info Project

---
+++ Student/src/sample/Test.java
@@ -0,0 +1,25 @@
+package sample;
+
+public class Test {
+
+	public static void main(String[] args) {
+		day2 S1=new day2();
+		S1.id=106;
+		System.out.println(S1.id);
+		S1.Name="Kunal";
+		System.out.println(S1.Name);
+		S1.Roll_no=17;
+		System.out.println(S1.Roll_no);
+		S1.Contact_No="7447899009";
+		System.out.println(S1.Contact_No);
+		S1.Course="Java Developer";
+		System.out.println(S1.Course);
+		S1.CGPA=7.7f;
+		System.out.println(S1.CGPA);
+		S1.skills="Full Stack Developer";
+		System.out.println(S1.skills);
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Student/src/sample/day2.java
@@ -0,0 +1,24 @@
+package sample;
+
+public class day2 {
+	
+
+	int id=106;
+	String Name="Kunal";
+	int Roll_no=17;
+	String Contact_No="7447899009";
+	String Course="Java Developer";
+	float CGPA=7.7f;
+	String skills="Full Stack Developer";
+	
+	
+	
+	
+	
+	
+	
+		// TODO Auto-generated method stub
+
+	}
+
+
